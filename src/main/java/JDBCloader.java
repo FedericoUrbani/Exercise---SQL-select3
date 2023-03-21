e0401330b9ede623954f83489b33a505551b7280
Federico Urbani <urbanifederico5@gmail.com>
2023-03-21 10:34:42 +0100

    updated

--- src/main/java/JDBCloader.java
+++ src/main/java/JDBCloader.java
@@ -16,21 +16,19 @@ public class JDBCloader {
             connection = DriverManager.getConnection(url, user, password);
             statement = connection.createStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS students (student_id int NOT NULL PRIMARY KEY AUTO_INCREMENT, last_name varchar(30) ,firstname varchar(30))");
-
-            /*   statement.execute("INSERT INTO newdb.students (last_name, firstname) VALUES ('rocchi', 'main')");
+            statement.execute("INSERT INTO newdb.students (last_name, firstname) VALUES ('rocchi', 'main')");
             statement.execute("INSERT INTO newdb.students (last_name, firstname) VALUES ('federici', 'menomain')");
             statement.execute("INSERT INTO newdb.students (last_name, firstname) VALUES ('rossi', 'main un tot')");
-            statement.execute("INSERT INTO newdb.students (last_name, firstname) VALUES ('fabrigna', 'rebecca')"); */
+            statement.execute("INSERT INTO newdb.students (last_name, firstname) VALUES ('fabrigna', 'rebecca')");
 
-            resultSet = statement.executeQuery("SELECT last_name FROM newdb.students");
+            resultSet = statement.executeQuery("SELECT * FROM students");
             while(resultSet.next()){
-
-                students.add(resultSet.getString(1));
+                System.out.println(resultSet.getString(2));
+                students.add(resultSet.getString(3));
 
             }
-            students.stream().forEach((surname) -> {
-                System.out.println(surname);
-            });
+            System.out.println(students);
+
 
         } catch (SQLException e) {
             e.printStackTrace();
