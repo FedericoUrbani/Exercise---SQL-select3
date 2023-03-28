d8b3ca091b5f9f3909187866e3a9881aa575de31
Federico Urbani <urbanifederico5@gmail.com>
2023-03-28 13:58:32 +0200

    updated view operation

--- src/main/java/JDBCloader.java
+++ src/main/java/JDBCloader.java
@@ -9,9 +9,11 @@ public class JDBCloader {
         String url = "jdbc:mysql://localhost:3306/newdb";
         String user = "developer";
         String password = "developer1";
-        ArrayList students = new ArrayList();
+        ArrayList italan_students = new ArrayList();
+        ArrayList german_students = new ArrayList();
 
 
+/*
         try {
             connection = DriverManager.getConnection(url, user, password);
             statement = connection.createStatement();
@@ -33,8 +35,10 @@ public class JDBCloader {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+*/
         try {
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
             //statement.execute("ALTER TABLE  students add country VARCHAR(30)");
             statement.execute("UPDATE students SET country = 'italy' WHERE student_id = 1");
             statement.execute("UPDATE students SET country = 'italy' WHERE student_id = 2");
@@ -45,6 +49,46 @@ public class JDBCloader {
             throw new RuntimeException(e);
         }
 
+        try {
+            statement.execute("CREATE or replace VIEW italian_students AS SELECT last_name, firstname FROM students WHERE country='italy';");
+            statement.execute("CREATE or replace VIEW german_students AS SELECT last_name, firstname FROM students WHERE country='germany';");
+            resultSet=statement.executeQuery("SELECT * FROM italian_students;");
+            while(resultSet.next()){
+                String name=resultSet.getString(1);
+                String surname=resultSet.getString(2);
+                Student stud= new Student(name,surname);
+                italan_students.add(stud);
+            }
+            for (int i=0;i<italan_students.size();i++) {
+                System.out.println(italan_students.get(i).toString());
+            }
+            resultSet=statement.executeQuery("SELECT * FROM german_students;");
+            while(resultSet.next()){
+                String name=resultSet.getString(1);
+                String surname=resultSet.getString(2);
+                Student stud= new Student(name,surname);
+                german_students.add(stud);
+            }
+            for (int i=0;i<german_students.size();i++) {
+                System.out.println(german_students.get(i).toString());
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
+
+/*
+create a class Student that has:
+2 string properties:
+name
+surname
+a constructor for setting the 2 values
+getters methods
+use JDBC for executing the following queries on the jdbc:mysql://localhost:3306/newdb database:
+create a view italian_students that gets all the name and surname of the Italian students
+create a view german_students that gets all the name and surname of the German students
+execute a select using the the italian_students and put the result in an ArrayList of Student objects called italianStudents
+execute a select using the the german_students and put the result in an ArrayList of Student objects called germanStudents
+ */
