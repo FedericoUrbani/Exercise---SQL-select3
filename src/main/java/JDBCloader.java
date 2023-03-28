1972610702ce81f3f6fd94b2b7eea324c6fc8153
Federico Urbani <urbanifederico5@gmail.com>
2023-03-28 11:45:39 +0200

    updated alter

--- src/main/java/JDBCloader.java
+++ src/main/java/JDBCloader.java
@@ -16,10 +16,10 @@ public class JDBCloader {
             connection = DriverManager.getConnection(url, user, password);
             statement = connection.createStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS students (student_id int NOT NULL PRIMARY KEY AUTO_INCREMENT, last_name varchar(30) ,firstname varchar(30))");
-            statement.execute("INSERT INTO newdb.students (last_name, firstname) VALUES ('rocchi', 'main')");
-            statement.execute("INSERT INTO newdb.students (last_name, firstname) VALUES ('federici', 'menomain')");
-            statement.execute("INSERT INTO newdb.students (last_name, firstname) VALUES ('rossi', 'main un tot')");
-            statement.execute("INSERT INTO newdb.students (last_name, firstname) VALUES ('fabrigna', 'rebecca')");
+            statement.execute("INSERT  INTO newdb.students (student_id,last_name, firstname) VALUES (1,'rocchi', 'main')");
+            statement.execute("INSERT INTO newdb.students (student_id,last_name, firstname) VALUES (2,'federici', 'menomain')");
+            statement.execute("INSERT INTO newdb.students (student_id,last_name, firstname) VALUES (3,'rossi', 'main un tot')");
+            statement.execute("INSERT INTO newdb.students (student_id,last_name, firstname) VALUES (4,'fabrigna', 'rebecca')");
 
             resultSet = statement.executeQuery("SELECT * FROM students");
             while(resultSet.next()){
@@ -33,6 +33,18 @@ public class JDBCloader {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+        try {
+            //statement.execute("ALTER TABLE  students add country VARCHAR(30)");
+            statement.execute("UPDATE students SET country = 'italy' WHERE student_id = 1");
+            statement.execute("UPDATE students SET country = 'italy' WHERE student_id = 2");
+            statement.execute("UPDATE students SET country = 'germany' WHERE student_id = 3");
+            statement.execute("UPDATE students SET country = 'germany' WHERE student_id = 4");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 }
